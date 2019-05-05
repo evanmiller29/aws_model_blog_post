268a43913755dea7a663265ac450fe4cc7f87478
evanmiller29 <evanmiller29@gmail.com>
2019-05-05 23:01:03 +0100

    Adding readme for blog post and uploading model building script

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# aws_model_blog_post
-Repository for hosting models on AWS blog post
+Hosting a ML model on AWS Lambdas + API Gateway
+So you've toiled forweeks (months cough cough)and you're beautiful machine learning model is done. Thank god.
+But now someone wants to use your model. That's great news ain't it? 
+It definitely is, but a whole new problem comes up. You've gotta put it some where and make it available to other people in the business. 
+This tutorial will show you how to use AWS to do this. In specific we'll be using the following components:
+API gateway
+S3 buckets
+Lambda functions/lambda layers
+
+I'm not using Sagemaker because it can be quite expensive £100 p/m per model opposed to lambda functions where you just pay per request.
+You'll need a few things to complete this. You'll need an installation of Python (scikit learn, pandas etc) and Docker installed. 
+Docker will be needed to build the correct lambda layer files if you are on a Windows machine (I learned this the hard way)
---
+++ build_model.py
@@ -0,0 +1,60 @@
+import pandas as pd
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.externals import joblib
+from sklearn.metrics import r2_score
+
+# Defining global variables
+
+rand_seed = 42
+
+# Reading in the data
+
+df = pd.read_csv('uk-housing-prices-paid/price_paid_records.csv', low_memory=True)
+print(df.columns.tolist())
+
+# Dropping all columns instead of price, property type, old/new, duration.
+# Some of these columns have odd input values, but since this is just a test example I'm not too worried about that.
+
+df = df[['Price', 'Property Type', 'Old/New', 'Duration']]
+
+# Outputting the possible values for the inputs because we'll need to know.
+
+print(df['Property Type'].value_counts())
+print(df['Old/New'].value_counts())
+print(df['Duration'].value_counts())
+
+# Splitting the data into train/test before building the model
+
+y = df['Price']
+X = df.drop('Price', axis=1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.33, random_state=rand_seed)
+
+# Building a pipeline to handle build a simple model. I'm just building a simple model, it probably won't be that accurate but that's OK.
+
+cat_featuress = ['Property Type', 'Old/New', 'Duration']
+cat_feat_processer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing')),
+    ('onehot', OneHotEncoder(handle_unknown='ignore'))]
+)
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', cat_feat_processer, cat_featuress)])
+
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('regressor', ElasticNet())
+    ]
+)
+
+model.fit(X_train, y_train)
+preds = model.predict(X_test)
+
+print(r2_score(y_test, preds))
